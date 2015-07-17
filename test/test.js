ad0f7bdaf556c0f38700f17fec0a2649f12343f1
Tom MacWright <tom@macwright.org>
2015-07-17 17:01:19 -0400

    Add directions support. Fixes #9

--- test/test.js
+++ test/test.js
@@ -131,3 +131,56 @@ test('MapboxClient#geocodeReverse', function(t) {
 
   t.end();
 });
+
+test('MapboxClient#directions', function(t) {
+  t.test('typecheck', function(t) {
+    var client = new MapboxClient(process.env.MapboxAccessToken);
+    t.ok(client);
+    t.throws(function() {
+      client.getDirections(null);
+    });
+    t.throws(function() {
+      client.getDirections(1, function() {});
+    });
+    t.throws(function() {
+      client.getDirections('foo', 1, function() {});
+    });
+    t.throws(function() {
+      client.getDirections('foo', 1);
+    });
+    t.end();
+  });
+
+  t.test('no options', function(t) {
+    var client = new MapboxClient(process.env.MapboxAccessToken);
+    t.ok(client);
+    client.getDirections([
+      { latitude: 33.6875431, longitude: -95.4431142 },
+      { latitude: 33.6875431, longitude: -95.4831142 }
+    ], function(err, results) {
+      t.ifError(err);
+      t.deepEqual(geojsonhint.hint(results.origin), [], 'origin is valid');
+      t.end();
+    });
+  });
+
+  t.test('all options', function(t) {
+    var client = new MapboxClient(process.env.MapboxAccessToken);
+    t.ok(client);
+    client.getDirections([
+      { latitude: 33.6875431, longitude: -95.4431142 },
+      { latitude: 33.6875431, longitude: -95.4831142 }
+    ], {
+      profile: 'mapbox.walking',
+      instructions: 'html',
+      alternatives: false,
+      geometry: 'polyline'
+    }, function(err, results) {
+      t.ifError(err);
+      t.deepEqual(geojsonhint.hint(results.origin), [], 'origin is valid');
+      t.end();
+    });
+  });
+
+  t.end();
+});
