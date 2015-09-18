97943a51c2bdceac1aee207222f8d55e0c3a6e2e
Tom MacWright <tom@macwright.org>
2015-09-18 10:42:44 -0700

    Add support for Static Maps API. Fixes #45
    cc @zeke - see test/services/static.js for how the API works

---
+++ test/encode_overlay.js
@@ -0,0 +1,50 @@
+/* eslint no-shadow: 0 */
+'use strict';
+
+var test = require('tap').test,
+  encodeOverlay = require('../lib/encode_overlay');
+
+test('encodeGeoJSON', function(t) {
+  t.throws(function() {
+    encodeOverlay.encodeGeoJSON({ type: 'invalid' });
+  });
+  t.equal(encodeOverlay.encodeGeoJSON({
+    type: 'Point',
+    coordinates: [0, 0]
+  }), 'geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B0%2C0%5D%7D)');
+  t.end();
+});
+
+test('encodePath', function(t) {
+  t.equal(encodeOverlay.encodePath({
+    geojson: { type: 'LineString', coordinates: [[0, 0], [1, 1]] }
+  }), 'path(??_ibE_ibE)');
+  t.equal(encodeOverlay.encodePath({
+    geojson: { type: 'LineString', coordinates: [[0, 0], [1, 1]] },
+    style: { strokecolor: 'fee' }
+  }), 'path+fee(??_ibE_ibE)');
+  t.equal(encodeOverlay.encodePath({
+    geojson: { type: 'LineString', coordinates: [[0, 0], [1, 1]] },
+    style: { strokewidth: 10, strokecolor: 'fee' }
+  }), 'path-10+fee(??_ibE_ibE)');
+  t.end();
+});
+
+test('encodeMarkers', function(t) {
+  t.equal(encodeOverlay.encodeMarkers([]), '');
+  t.equal(encodeOverlay.encodeMarkers([{
+    longitude: 0, latitude: 0
+  }]), 'pin-l-circle(0,0)');
+  t.equal(encodeOverlay.encodeMarkers([{
+    longitude: 0, latitude: 0, size: 'm'
+  }]), 'pin-m-circle(0,0)');
+  t.equal(encodeOverlay.encodeMarkers([{
+    longitude: 0, latitude: 0, symbol: 'city'
+  }]), 'pin-l-city(0,0)');
+  t.equal(encodeOverlay.encodeMarkers([{
+    longitude: 0, latitude: 0, symbol: 'city'
+  }, {
+    longitude: 10, latitude: 2, symbol: 'circle'
+  }]), 'pin-l-city(0,0),pin-l-circle(10,2)');
+  t.end();
+});
---
+++ test/static.js
@@ -0,0 +1,56 @@
+/* eslint no-shadow: 0 */
+'use strict';
+
+var test = require('tap').test;
+var MapboxClient = require('../lib/services/static');
+
+function removeToken(url) {
+  return url.replace(/\?access_token.*$/, '');
+}
+
+test('MapboxStatic', function(t) {
+  var client = new MapboxClient(process.env.MapboxAccessToken);
+  t.throws(function() { client.getStaticURL(); });
+  t.throws(function() { client.getStaticURL('foo'); });
+  t.throws(function() { client.getStaticURL('foo', 10); });
+  t.throws(function() { client.getStaticURL('foo', 'foo', 10); });
+
+  t.equal(removeToken(client.getStaticURL('foo', 10, 10, {
+    longitude: 1, latitude: 2, zoom: 3
+  })), 'https://api.mapbox.com/v4/foo/1,2,3/10x10.png', 'basic url');
+
+  t.equal(removeToken(client.getStaticURL('foo', 10, 10, {
+    longitude: 1, latitude: 2, zoom: 3
+  }, {
+    format: 'jpg80'
+  })), 'https://api.mapbox.com/v4/foo/1,2,3/10x10.jpg80', 'format option');
+
+  t.equal(removeToken(client.getStaticURL('foo', 10, 10, {
+    longitude: 1, latitude: 2, zoom: 3
+  }, {
+    retina: true
+  })), 'https://api.mapbox.com/v4/foo/1,2,3/10x10@2x.png', 'retina option');
+
+  t.equal(removeToken(client.getStaticURL('foo', 10, 10, {
+    longitude: 1, latitude: 2, zoom: 3
+  }, {
+    retina: true,
+    markers: [{ longitude: 1, latitude: 2 }]
+  })), 'https://api.mapbox.com/v4/foo/pin-l-circle(1,2)/1,2,3/10x10@2x.png', 'with markers');
+
+  t.equal(removeToken(client.getStaticURL('foo', 10, 10, {
+    longitude: 1, latitude: 2, zoom: 3
+  }, {
+    retina: true,
+    geojson: { type: 'Point', coordinates: [0, 0] }
+  })), 'https://api.mapbox.com/v4/foo/geojson(%257B%2522type%2522%253A%2522Point%2522%252C%2522coordinates%2522%253A%255B0%252C0%255D%257D)/1,2,3/10x10@2x.png', 'with geojson');
+
+  t.equal(removeToken(client.getStaticURL('foo', 10, 10, {
+    longitude: 1, latitude: 2, zoom: 3
+  }, {
+    retina: true,
+    path: { geojson: { type: 'LineString', coordinates: [[0, 0], [1, 1]] } }
+  })), 'https://api.mapbox.com/v4/foo/path(??_ibE_ibE)/1,2,3/10x10@2x.png', 'with path');
+
+  t.end();
+});
