2b2c540c7850a7732f3e273c909a7d8f3f094216
Scott Andrews <scott@mapbox.com>
2017-05-08 17:30:36 -0700

    Tileset polish
    - define access_token in URL
    - make callback functions optional in jsdoc
    - add limit property for tileset listing
    - add test for tileset listing pagination
    - add test for tilequery

--- test/tilesets.js
+++ test/tilesets.js
@@ -6,12 +6,6 @@ var MapboxClient = require('../lib/services/tilesets');
 
 test('TilesetClient', function(tilesetClient) {
 
-  if (process.browser) {
-    tilesetClient.pass('skipping tileset api in browser');
-    return tilesetClient.end();
-  }
-
-
   tilesetClient.test('#listTilesets', function(listTilesets) {
 
     listTilesets.test('simple list', function(assert) {
@@ -27,7 +21,37 @@ test('TilesetClient', function(tilesetClient) {
       });
     });
 
+    // NOTE this test may fail when run in a clean account
+    listTilesets.test('paginated list', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      assert.ok(client, 'created tileset client');
+      client.listTilesets({ limit: 1 }, function(err, tilesets, response) {
+        assert.ifError(err, 'success');
+        assert.equal(tilesets.length, 1);
+        assert.equal(typeof response.nextPage, 'function', 'has another page');
+        assert.end();
+      });
+    });
+
     listTilesets.end();
+  });
+
+  tilesetClient.test('#tilequery', function(tilequery) {
+
+    tilequery.test('simple query', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      assert.ok(client, 'created tileset client');
+      client.tilequery('mapbox.mapbox-streets-v7', [0, 0], {}, function(err, results) {
+        assert.ifError(err, 'success');
+        assert.ok(Array.isArray(results.features), 'lists features');
+        assert.ok(results.features.some(function (feature) {
+          return feature.properties.tilequery.layer === 'water';
+        }));
+        assert.end();
+      });
+    });
+
+    tilequery.end();
   });
 
   tilesetClient.end();
