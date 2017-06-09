abcfeafd4e34b7d94e9407187476bd233309349f
geografa <rafa@mapbox.com>
2017-06-09 10:54:38 -0700

    fixes options for matrix service. updates test

--- test/matrix.js
+++ test/matrix.js
@@ -26,18 +26,11 @@ test('MapboxClient#getMatrix', function(t) {
   t.test('no options', function(t) {
     var client = new MapboxClient(process.env.MapboxAccessToken);
     t.ok(client);
-    client.getMatrix([{ 
-      longitude: -122.42,
-      latitude: 37.78
-    },
-    { 
-      longitude: -122.45,
-      latitude: 37.91
-    },
-    { 
-      longitude: -122.48,
-      latitude: 37.73
-    }],{ profile: 'walking'}, function(err, results) {
+    client.getMatrix([
+      { longitude: -122.42, latitude: 37.78 },
+      { longitude: -122.45, latitude: 37.91 },
+      { longitude: -122.48, latitude: 37.73 }
+    ], function(err, results) {
       t.ifError(err);
       t.ok(Array.isArray(results.durations), 'returns an array');
       t.equal(results.durations.length, 3, 'array has correct dimension');
@@ -46,25 +39,24 @@ test('MapboxClient#getMatrix', function(t) {
     });
   });
 
-  // t.test('profile option', function(t) {
-  //   var client = new MapboxClient(process.env.MapboxAccessToken);
-  //   t.ok(client);
-  //   client.getMatrix([
-  //     [-95.4431142, 33.6875431],
-  //     [-95.4831142, 33.6875431],
-  //     [-95.4831142, 33.2875431],
-  //     [-95.4831142, 33.3875431],
-  //     [-95.4831142, 33.0875431]
-  //   ], {
-  //     profile: 'walking'
-  //   }, function(err, results) {
-  //     t.ifError(err);
-  //     t.ok(Array.isArray(results.durations), 'returns an array');
-  //     t.equal(results.durations.length, 5, 'array has correct dimension');
-  //     t.equal(results.durations[0].length, 5, 'array has correct dimension');
-  //     t.end();
-  //   });
-  // });
+  t.test('all options', function(t) {
+    var client = new MapboxClient(process.env.MapboxAccessToken);
+    t.ok(client);
+    client.getMatrix([
+      { longitude: -122.42, latitude: 37.78 },
+      { longitude: -122.45, latitude: 37.91 },
+      { longitude: -122.48, latitude: 37.73 }
+    ], {
+      profile: 'walking'
+    }, function(err, results) {
+      console.log(err);
+      t.ifError(err);
+      t.ok(Array.isArray(results.durations), 'returns an array');
+      t.equal(results.durations.length, 3, 'array has correct dimension');
+      t.equal(results.durations[0].length, 3, 'array has correct dimension');
+      t.end();
+    });
+  });
 
   t.end();
 });
