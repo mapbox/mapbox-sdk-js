985d01feddbff3edfe9a1dedec58321c40c0d996
David Clark <dclark@mapbox.com>
2018-05-09 13:14:31 -0700

    Create service clients without first creating a base client

--- services/service-helpers/create-service-factory.js
+++ services/service-helpers/create-service-factory.js
@@ -1,7 +1,17 @@
 'use strict';
 
+var MapiClient = require('../../lib/classes/mapi-client');
+// This will create the environment-appropriate client.
+var createClient = require('../../lib/client');
+
 function createServiceFactory(ServicePrototype) {
-  return function(client) {
+  return function(clientOrConfig) {
+    var client;
+    if (MapiClient.prototype.isPrototypeOf(clientOrConfig)) {
+      client = clientOrConfig;
+    } else {
+      client = createClient(clientOrConfig);
+    }
     var service = Object.create(ServicePrototype);
     service.client = client;
     return service;
