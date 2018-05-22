3f60150c212eb839324ac0c1ccecff62bb758370
Kushan Joshi <0o3ko0@gmail.com>
2018-05-22 19:16:39 +0530

    make pick accept a function

--- services/service-helpers/pick.js
+++ services/service-helpers/pick.js
@@ -2,18 +2,29 @@
 
 /**
  * Create a new object by picking properties off an existing object.
- *
+ * The second param can be overloaded as a callback for
+ * more fine grained picking of properties.
  * @param {Object} source
- * @param {Array<string>} keys
+ * @param {Array<string>|function(string, Object):boolean} keys
  * @returns {Object}
  */
 function pick(source, keys) {
-  return Object.keys(source).reduce(function(result, key) {
-    if (keys.indexOf(key) !== -1 && source[key] !== undefined) {
+  var filter = function(key, val) {
+    return keys.indexOf(key) !== -1 && val !== undefined;
+  };
+
+  if (typeof keys === 'function') {
+    filter = keys;
+  }
+
+  return Object.keys(source)
+    .filter(function(key) {
+      return filter(key, source[key]);
+    })
+    .reduce(function(result, key) {
       result[key] = source[key];
-    }
-    return result;
-  }, {});
+      return result;
+    }, {});
 }
 
 module.exports = pick;
