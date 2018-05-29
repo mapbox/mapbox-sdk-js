f3d3ae3e40ba715dc582b26fcad5187cd3ca0cdd
Kushan Joshi <0o3ko0@gmail.com>
2018-05-29 16:56:49 +0530

    add matrix

---
+++ services/__tests__/matrix.test.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const matrixService = require('../matrix');
+const tu = require('../../test/test-utils');
+
+let matrix;
+beforeEach(() => {
+  matrix = matrixService(tu.mockClient());
+});
+
+describe('getMatrix', () => {
+  test('works', () => {
+    matrix.getMatrix({
+      matrixPath: [
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [2.2, 1.1]
+        }
+      ]
+    });
+    expect(tu.requestConfig(matrix)).toEqual({
+      path: '/directions-matrix/v1/mapbox/:profile/:coordinates',
+      method: 'GET',
+      params: {
+        coordinates: '2.2,1.1;2.2,1.1',
+        profile: 'driving'
+      },
+      query: {}
+    });
+  });
+
+  test('it understands approach', () => {
+    matrix.getMatrix({
+      matrixPath: [
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [2.2, 1.1],
+          approach: 'curb'
+        },
+        {
+          coordinates: [3.2, 1.1]
+        },
+        {
+          coordinates: [4.2, 1.1]
+        }
+      ],
+      profile: 'walking'
+    });
+    expect(tu.requestConfig(matrix)).toEqual({
+      path: '/directions-matrix/v1/mapbox/:profile/:coordinates',
+      method: 'GET',
+      params: {
+        coordinates: '2.2,1.1;2.2,1.1;3.2,1.1;4.2,1.1',
+        profile: 'walking'
+      },
+      query: {
+        approaches: ';curb;;'
+      }
+    });
+  });
+
+  test('understands annotations ', () => {
+    matrix.getMatrix({
+      matrixPath: [
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [3.2, 1.1]
+        },
+        {
+          coordinates: [4.2, 1.1]
+        }
+      ],
+      profile: 'walking',
+      annotations: ['distance', 'duration']
+    });
+    expect(tu.requestConfig(matrix)).toEqual({
+      path: '/directions-matrix/v1/mapbox/:profile/:coordinates',
+      method: 'GET',
+      params: {
+        coordinates: '2.2,1.1;2.2,1.1;3.2,1.1;4.2,1.1',
+        profile: 'walking'
+      },
+      query: {
+        annotations: 'distance,duration'
+      }
+    });
+  });
+
+  test('handles when sources & destinations are array', () => {
+    matrix.getMatrix({
+      matrixPath: [
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [3.2, 1.1]
+        },
+        {
+          coordinates: [4.2, 1.1]
+        }
+      ],
+      profile: 'walking',
+      sources: [1, 5],
+      destinations: [0, 2]
+    });
+    expect(tu.requestConfig(matrix)).toEqual({
+      path: '/directions-matrix/v1/mapbox/:profile/:coordinates',
+      method: 'GET',
+      params: {
+        coordinates: '2.2,1.1;2.2,1.1;3.2,1.1;4.2,1.1',
+        profile: 'walking'
+      },
+      query: {
+        destinations: '0;2',
+        sources: '1;5'
+      }
+    });
+  });
+
+  test('handles when sources & destinations are `all`', () => {
+    matrix.getMatrix({
+      matrixPath: [
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [2.2, 1.1]
+        },
+        {
+          coordinates: [3.2, 1.1]
+        },
+        {
+          coordinates: [4.2, 1.1]
+        }
+      ],
+      profile: 'walking',
+      sources: 'all',
+      destinations: 'all'
+    });
+    expect(tu.requestConfig(matrix)).toEqual({
+      path: '/directions-matrix/v1/mapbox/:profile/:coordinates',
+      method: 'GET',
+      params: {
+        coordinates: '2.2,1.1;2.2,1.1;3.2,1.1;4.2,1.1',
+        profile: 'walking'
+      },
+      query: {
+        destinations: 'all',
+        sources: 'all'
+      }
+    });
+  });
+});
