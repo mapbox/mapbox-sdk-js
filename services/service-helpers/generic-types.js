f116e303737877a6fa6c8348e1112e0d5c2b828e
David Clark <dclark@mapbox.com>
2018-06-05 21:13:38 -0700

    Polish Styles service

--- services/service-helpers/generic-types.js
+++ services/service-helpers/generic-types.js
@@ -9,3 +9,11 @@
  *
  * @typedef {Array<number>} BoundingBox
  */
+
+/**
+ * In Node, files must be `ReadableStream`s or paths pointing for the file in the filesystem.
+ *
+ * In the browser, files must be `Blob`s or `ArrayBuffer`s.
+ *
+ * @typedef {Blob|ArrayBuffer|string|ReadableStream} UploadableFile
+ */
