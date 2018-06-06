560ad3c3503414b869cd10b70956ecc65fc95e0f
David Clark <dclark@mapbox.com>
2018-06-05 21:19:04 -0700

    Polish Tilequery service

--- services/tilequery.js
+++ services/tilequery.js
@@ -6,33 +6,32 @@ var createServiceFactory = require('./service-helpers/create-service-factory');
 
 /**
  * Tilequery API service.
+ *
+ * Learn more about this service and its responses in
+ * [the HTTP service documentation](https://www.mapbox.com/api-documentation/#tilequery).
  */
 var Tilequery = {};
 
 /**
  * List features within a radius of a point on a map (or several maps).
  *
- * See the [public documentation](https://www.mapbox.com/api-documentation/#tilequery).
- *
  * @param {Object} config
- * @param {string|Array<string>} config.mapId
- * @param {number} config.longitude
- * @param {number} config.latitude
+ * @param {Array<string>} config.mapIds - The maps being queried.
+ *   If you need to composite multiple layers, provide multiple map IDs.
+ * @param {Coordinates} config.coordinates - The longitude and latitude to be queried.
  * @param {number} [config.radius=0] - The approximate distance in meters to query for features.
  * @param {number} [config.limit=5] - The number of features to return, between 1 and 50.
  * @param {boolean} [config.dedupe=true] - Whether or not to deduplicate results.
- * @param {'polygon'|'linestring'|'point'} [config.geometry] - Search only for the specified
- *   geometry types.
+ * @param {'polygon'|'linestring'|'point'} [config.geometry] - Queries for a specific geometry type.
  * @param {Array<string>} [config.layers] - IDs of vector layers to query.
  * @return {MapiRequest}
  */
 Tilequery.listFeatures = function(config) {
   v.assertShape({
-    mapId: v.required(v.oneOfType(v.string, v.arrayOf(v.string))),
-    longitude: v.required(v.number),
-    latitude: v.required(v.number),
+    mapIds: v.required(v.arrayOf(v.string)),
+    coordinates: v.required(v.coordinates),
     radius: v.number,
-    limit: v.number,
+    limit: v.range([1, 50]),
     dedupe: v.boolean,
     layers: v.arrayOf(v.string)
   })(config);
@@ -41,8 +40,8 @@ Tilequery.listFeatures = function(config) {
     method: 'GET',
     path: '/v4/:mapIds/tilequery/:coordinates.json',
     params: {
-      mapIds: [].concat(config.mapId),
-      coordinates: [config.longitude, config.latitude]
+      mapIds: config.mapIds,
+      coordinates: config.coordinates
     },
     query: pick(config, ['radius', 'limit', 'dedupe', 'layers'])
   });
