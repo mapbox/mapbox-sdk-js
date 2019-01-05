c5bc1b62185746b970ee173a926815c98e5a4497
HeyStenson <heather.stenson@mapbox.com>
2019-01-04 17:11:23 -0800

    .

--- services/static.js
+++ services/static.js
@@ -9,7 +9,7 @@ var pick = require('./service-helpers/pick');
  * Static API service.
  *
  * Learn more about this service and its responses in
- * [the HTTP service documentation](https://www.mapbox.com/api-documentation/maps/#static).
+ * [the HTTP service documentation](https://www.mapbox.com/api-documentation/maps/#stati).
  */
 var Static = {};
 
