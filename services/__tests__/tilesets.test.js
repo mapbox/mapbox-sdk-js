489340bdc9bc6be0cfe3bec81224f1486581ebdf
Andrew Harvey <andrew@alantgeo.com.au>
2020-01-30 15:03:21 +1100

    add unit tests

--- services/__tests__/tilesets.test.js
+++ services/__tests__/tilesets.test.js
@@ -34,6 +34,17 @@ describe('listTilesets', () => {
   });
 });
 
+describe('deleteTileset', () => {
+  test('works', () => {
+    tilesets.deleteTileset({ tilesetId: 'hello-world' });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId',
+      method: 'DELETE',
+      params: { tilesetId: 'hello-world' }
+    });
+  });
+});
+
 describe('tileJSONMetadata', () => {
   test('works', () => {
     tilesets.tileJSONMetadata({ tilesetId: 'hello-world' });
@@ -55,16 +66,17 @@ describe('createTilesetSource', () => {
       path: '/tilesets/v1/sources/:ownerId/:id',
       method: 'POST',
       params: {
-        id: 'tileset_source_id',
-        file: 'path/to/file.geojson'
-      }
+        id: 'tileset_source_id'
+      },
+      file: 'path/to/file.geojson',
+      sendFileAs: 'form'
     });
   });
 
   test('works with specified ownerId', () => {
     tilesets.createTilesetSource({
       ownerId: 'specialguy',
-      id: 'tileset_id',
+      id: 'tileset_source_id',
       file: 'path/to/file.geojson'
     });
     expect(tu.requestConfig(tilesets)).toEqual({
@@ -72,9 +84,271 @@ describe('createTilesetSource', () => {
       method: 'POST',
       params: {
         ownerId: 'specialguy',
-        id: 'tileset_id',
-        file: 'path/to/file.geojson'
+        id: 'tileset_source_id'
+      },
+      file: 'path/to/file.geojson',
+      sendFileAs: 'form'
+    });
+  });
+});
+
+describe('getTilesetSource', () => {
+  test('works', () => {
+    tilesets.getTilesetSource({
+      id: 'tileset_source_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/sources/:ownerId/:id',
+      method: 'GET',
+      params: {
+        id: 'tileset_source_id'
       }
+    });
+  });
+
+  test('works with specified ownerId', () => {
+    tilesets.getTilesetSource({
+      ownerId: 'specialguy',
+      id: 'tileset_source_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/sources/:ownerId/:id',
+      method: 'GET',
+      params: {
+        ownerId: 'specialguy',
+        id: 'tileset_source_id'
+      }
+    });
+  });
+});
+
+describe('listTilesetSources', () => {
+  test('works', () => {
+    tilesets.listTilesetSources();
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/sources/:ownerId',
+      method: 'GET',
+      params: {}
+    });
+  });
+
+  test('works with specified ownerId', () => {
+    tilesets.listTilesetSources({
+      ownerId: 'specialguy'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/sources/:ownerId',
+      method: 'GET',
+      params: {
+        ownerId: 'specialguy'
+      }
+    });
+  });
+});
+
+describe('deleteTilesetSource', () => {
+  test('works', () => {
+    tilesets.deleteTilesetSource({
+      id: 'tileset_source_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/sources/:ownerId/:id',
+      method: 'DELETE',
+      params: {
+        id: 'tileset_source_id'
+      }
+    });
+  });
+
+  test('works with specified ownerId', () => {
+    tilesets.deleteTilesetSource({
+      ownerId: 'specialguy',
+      id: 'tileset_source_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/sources/:ownerId/:id',
+      method: 'DELETE',
+      params: {
+        ownerId: 'specialguy',
+        id: 'tileset_source_id'
+      }
+    });
+  });
+});
+
+describe('createTileset', () => {
+  test('works', () => {
+    tilesets.createTileset({
+      tilesetId: 'tileset_id',
+      recipe: { version: 1 },
+      name: 'Tileset Name'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId',
+      method: 'POST',
+      params: {
+        tilesetId: 'tileset_id'
+      },
+      body: {
+        recipe: { version: 1 },
+        name: 'Tileset Name'
+      }
+    });
+  });
+
+  test('works with specified private and description', () => {
+    tilesets.createTileset({
+      tilesetId: 'tileset_id',
+      recipe: { version: 1 },
+      name: 'Tileset Name',
+      private: false,
+      description: 'Tileset Description'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId',
+      method: 'POST',
+      params: {
+        tilesetId: 'tileset_id'
+      },
+      body: {
+        recipe: { version: 1 },
+        name: 'Tileset Name',
+        private: false,
+        description: 'Tileset Description'
+      }
+    });
+  });
+});
+
+describe('publishTileset', () => {
+  test('works', () => {
+    tilesets.publishTileset({
+      tilesetId: 'tileset_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/publish',
+      method: 'POST',
+      params: {
+        tilesetId: 'tileset_id'
+      }
+    });
+  });
+});
+
+describe('tilesetStatus', () => {
+  test('works', () => {
+    tilesets.tilesetStatus({
+      tilesetId: 'tileset_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/status',
+      method: 'GET',
+      params: {
+        tilesetId: 'tileset_id'
+      }
+    });
+  });
+});
+
+describe('tilesetJob', () => {
+  test('works', () => {
+    tilesets.tilesetJob({
+      tilesetId: 'tileset_id',
+      jobId: 'job_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/jobs/:jobId',
+      method: 'GET',
+      params: {
+        tilesetId: 'tileset_id',
+        jobId: 'job_id'
+      }
+    });
+  });
+});
+
+describe('listTilesetJobs', () => {
+  test('works', () => {
+    tilesets.listTilesetJobs({
+      tilesetId: 'tileset_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/jobs',
+      method: 'GET',
+      params: {
+        tilesetId: 'tileset_id'
+      },
+      query: {}
+    });
+  });
+
+  test('works with stage', () => {
+    tilesets.listTilesetJobs({
+      tilesetId: 'tileset_id',
+      stage: 'success'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/jobs',
+      method: 'GET',
+      params: {
+        tilesetId: 'tileset_id'
+      },
+      query: {
+        stage: 'success'
+      }
+    });
+  });
+});
+
+describe('getTilesetsQueue', () => {
+  test('works', () => {
+    tilesets.getTilesetsQueue();
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/queue',
+      method: 'PUT'
+    });
+  });
+});
+
+describe('validateRecipe', () => {
+  test('works', () => {
+    tilesets.validateRecipe({
+      recipe: { version: 1 }
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/validateRecipe',
+      method: 'PUT',
+      body: { version: 1 }
+    });
+  });
+});
+
+describe('getRecipe', () => {
+  test('works', () => {
+    tilesets.getRecipe({
+      tilesetId: 'tileset_id'
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/recipe',
+      method: 'GET',
+      params: {
+        tilesetId: 'tileset_id'
+      }
+    });
+  });
+});
+
+describe('updateRecipe', () => {
+  test('works', () => {
+    tilesets.updateRecipe({
+      tilesetId: 'tileset_id',
+      recipe: { version: 1 }
+    });
+    expect(tu.requestConfig(tilesets)).toEqual({
+      path: '/tilesets/v1/:tilesetId/recipe',
+      method: 'PATCH',
+      params: { tilesetId: 'tileset_id' },
+      body: { version: 1 }
     });
   });
 });
