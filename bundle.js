de3b460fa0cfb04971a55e672405fb9e4a63959f
Andrew Harvey <andrew@alantgeo.com.au>
2021-06-11 13:04:25 +1000

    alphabetic order

--- bundle.js
+++ bundle.js
@@ -3,13 +3,13 @@
 var browserClient = require('./lib/browser/browser-client');
 var mbxDatasets = require('./services/datasets');
 var mbxDirections = require('./services/directions');
+var mbxFonts = require('./services/fonts');
 var mbxGeocoding = require('./services/geocoding');
 var mbxMapMatching = require('./services/map-matching');
 var mbxMatrix = require('./services/matrix');
 var mbxOptimization = require('./services/optimization');
 var mbxStatic = require('./services/static');
 var mbxStyles = require('./services/styles');
-var mbxFonts = require('./services/fonts');
 var mbxTilequery = require('./services/tilequery');
 var mbxTilesets = require('./services/tilesets');
 var mbxTokens = require('./services/tokens');
@@ -21,13 +21,13 @@ function mapboxSdk(options) {
 
   client.datasets = mbxDatasets(client);
   client.directions = mbxDirections(client);
+  client.fonts = mbxFonts(client);
   client.geocoding = mbxGeocoding(client);
   client.mapMatching = mbxMapMatching(client);
   client.matrix = mbxMatrix(client);
   client.optimization = mbxOptimization(client);
   client.static = mbxStatic(client);
   client.styles = mbxStyles(client);
-  client.fonts = mbxFonts(client);
   client.tilequery = mbxTilequery(client);
   client.tilesets = mbxTilesets(client);
   client.tokens = mbxTokens(client);
