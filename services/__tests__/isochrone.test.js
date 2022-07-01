21372d7ad4d18625d59d2ea85c522fc99e8cf06d
Grigory Dobrov <grigory.dobrov@mapbox.com>
2022-07-01 16:43:04 +0300

    support for contour_meters Isochrone API param mapbox/playground#193

--- services/__tests__/isochrone.test.js
+++ services/__tests__/isochrone.test.js
@@ -51,7 +51,7 @@ describe('getContours', () => {
     });
   });
 
-  test('with all config options', () => {
+  test('with all config options (minutes)', () => {
     isochrone.getContours({
       coordinates: [-118.22258, 33.99038],
       minutes: [5, 10, 15, 20],
@@ -71,6 +71,34 @@ describe('getContours', () => {
       },
       query: {
         contours_minutes: '5,10,15,20',
+        polygons: 'true',
+        contours_colors: '6706ce,04e813,4286f4,555555',
+        denoise: 0,
+        generalize: 0
+      }
+    });
+  });
+
+  test('with all config options (meters)', () => {
+    isochrone.getContours({
+      coordinates: [-118.22258, 33.99038],
+      meters: [5000, 10000, 15000, 20000],
+      profile: 'walking',
+      polygons: true,
+      colors: ['6706ce', '04e813', '4286f4', '555555'],
+      denoise: 0,
+      generalize: 0
+    });
+
+    expect(tu.requestConfig(isochrone)).toEqual({
+      path: '/isochrone/v1/mapbox/:profile/:coordinates',
+      method: 'GET',
+      params: {
+        coordinates: '-118.22258,33.99038',
+        profile: 'walking'
+      },
+      query: {
+        contours_meters: '5000,10000,15000,20000',
         polygons: 'true',
         contours_colors: '6706ce,04e813,4286f4,555555',
         denoise: 0,
@@ -109,6 +137,15 @@ describe('getContours', () => {
     ).toThrow('minutes must contain between 1 and 4 contour values');
   });
 
+  test('errors if more than 4 contours_meters requested', () => {
+    expect(() =>
+      isochrone.getContours({
+        coordinates: [-118.22258, 33.99038],
+        meters: [5000, 10000, 15000, 20000, 30000]
+      })
+    ).toThrow('meters must contain between 1 and 4 contour values');
+  });
+
   test('errors if minute value is greater than 60', () => {
     expect(() =>
       isochrone.getContours({
@@ -116,6 +153,15 @@ describe('getContours', () => {
         minutes: [40, 50, 60, 70]
       })
     ).toThrow('minutes must be less than 60');
+  });
+
+  test('errors if meter value is greater than 100000', () => {
+    expect(() =>
+      isochrone.getContours({
+        coordinates: [-118.22258, 33.99038],
+        meters: [10000, 50000, 100000, 100001]
+      })
+    ).toThrow('meters must be less than 100000');
   });
 
   test('errors if generalize is less than 0', () => {
@@ -137,5 +183,27 @@ describe('getContours', () => {
         colors: ['6706ce', '04e813']
       })
     ).toThrow('colors should have the same number of entries as minutes');
+  });
+
+  test('colors should have the same number of entries as meters', () => {
+    expect(() =>
+      isochrone.getContours({
+        coordinates: [-118.22258, 33.99038],
+        meters: [5000, 10000, 15000, 20000],
+        profile: 'walking',
+        colors: ['6706ce', '04e813']
+      })
+    ).toThrow('colors should have the same number of entries as meters');
+  });
+
+  test('either meters or minutes should be specified', () => {
+    expect(() =>
+      isochrone.getContours({
+        coordinates: [-118.22258, 33.99038],
+        minutes: [5, 10, 15, 20],
+        meters: [5000, 10000, 15000, 20000],
+        profile: 'walking'
+      })
+    ).toThrow("minutes and meters can't be specified at the same time");
   });
 });
